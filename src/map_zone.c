8d4fea6c511dacf5e693004518dbf1dcede6bdeb
ljoly <ljoly@student.42.fr>
2019-02-21 18:10:05 +0100

    Fix bad region size updating (2)

--- src/map_zone.c
+++ src/map_zone.c
@@ -6,7 +6,7 @@
 /*   By: ljoly <ljoly@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/11 20:32:13 by ljoly             #+#    #+#             */
-/*   Updated: 2019/02/20 10:42:13 by ljoly            ###   ########.fr       */
+/*   Updated: 2019/02/21 17:52:58 by ljoly            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,22 +39,36 @@ void			map_zone(t_req *r, t_type type, t_bool new_block)
 
 		g_meta[i].type = type;
 		g_meta[i].ptr = mmap(0, r->region_size, MMAP_FLAGS, -1, 0);
-		g_meta[i].size = r->size_to_map;
+		g_meta[i].size = r->region_size;
+		// g_meta[i].size = r->size_to_map;
 		g_meta[i].size_request = r->region_size;
 		r->zone = g_meta[i].ptr;
 		g_meta[0].size--;
 	}
 	else if (new_block)
 	{
-		ft_putstr("Need to add Block ");
-		ft_putnbr(i - 1);
-		ft_putchar('\n');
+		// ft_putstr("Need to add Block ");
+		// ft_putnbr(i - 1);
+		// ft_putchar('\n');
 		g_meta[i].type = type;
 		g_meta[i].ptr = r->zone;
 		g_meta[i].size = r->size_to_map;
 		g_meta[i].size_request = r->size_request;
+		// 		ft_putstr("size to remove = ");
+		// ft_putnbr(r->size_to_map);
+		// ft_putchar('\n');
+		g_meta[r->region_index].size -= r->size_to_map;
+		// ft_putstr("r->region_index = ");
+		// ft_putnbr(r->region_index);
+		// ft_putchar('\n');
+		// ft_putstr("meta.[r->region_index].size = ");
+		// ft_putnbr(g_meta[r->region_index].size);
+		// ft_putchar('\n');
 		g_meta[0].size--;
 	}
 	else
+	{
+		g_meta[r->region_index].size -= g_meta[r->index].size;
 		update_block(r, type);
+	}
 }
